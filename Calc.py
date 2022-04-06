769f7071ba2ad8f59d02b81a7803672cc0ecbcf7
João Pedro <102665587+Rigimon@users.noreply.github.com>
2022-04-06 07:30:25 -0300

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,6 +1,6 @@
 print ("Calculadora");
-n1 = input();
-n2 = input();
+n1 = int(input());
+n2 = int(input());
 print();
 print ("Calculadora");
 print();
@@ -8,14 +8,14 @@ print ("(1)Soma");
 print ("(2)Subtração");
 print ("(3)Multiplicação");
 print ("(4)Divisão");
-op = input();
+op = int(input());
 if op == 1:
     print (n1,"+",n2,"=",n1+n2);
 elif op == 2:
-    print (n1-n2);
+    print (n1,"-",n2,"=",n1-n2);
 elif op == 3:
-    print (n1*n2);
+    print (n1,"*",n2,"=",n1*n2);
 elif (op == 4):
-    print (n1/n2);
+    print (n1,"/",n2,"=",n1/n2);
 else:
     print ("Digite o numero certo fdp");
